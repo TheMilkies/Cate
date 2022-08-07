4a4e261db2f166dbfcd385496ab4fe837a67e509
KrystalKX <themilkiesyes@gmail.com>
2022-08-07 18:32:58 +0300

    thread count stuff!! onlly need to actually optimize the build

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,15 @@
 
 bool parser_exit;
 
-int thread_count = 4;
+int thread_count = 2;
 
-int main(int argc, const char *argv[])
+inline bool ends_with(std::string const & value, std::string const & ending) //written by tshepang from stackoverflow
+{
+    if (ending.size() > value.size()) return false;
+    return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
+}
+
+int main(int argc, char *argv[])
 {
 	std::ios_base::sync_with_stdio(false); // because SPEED
 
@@ -14,7 +20,26 @@ int main(int argc, const char *argv[])
 		return 1;
 	}
 
-	Parser parser(argv[1]);
+	char*file_name;
+
+	for (int i = 1; i < argc; i++)
+	{
+		string arg = argv[i];
+		if (arg.rfind("-t", 0) == 0)//if starts with
+		{
+			thread_count= std::stoi(arg.substr(2, arg.length()));
+		}
+		else
+		{
+			if (ends_with(arg, ".cate"))
+				file_name = (char*)argv[i];
+			else //add .cate ending
+				file_name = strcat(argv[i], ".cate");
+		}
+	
+	}
+
+	Parser parser(file_name);
 	parser.parse();
 
 	return 0;
