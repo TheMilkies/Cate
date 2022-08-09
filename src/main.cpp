3a24e8df52eae8dc0a6ebb5971f6cbf6bb1e0bbe
Yogurt <themilkiesyes@gmail.com>
2022-08-09 20:27:00 +0300

    updated command parsing!

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "Parser.hpp"
 
-bool parser_exit;
+bool parser_exit, system_allowed = true;
 
 int thread_count = std::thread::hardware_concurrency() * 2;
 
@@ -10,14 +10,24 @@ inline bool ends_with(std::string const & value, std::string const & ending) //w
     return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
 }
 
+void help()
+{
+	std::cout << "\x1b[34mCate " CATE_VERSION "\x1b[0m\n"
+	BOLD "usage:" RESET "\tcate [FLAGS] [FILENAME]\n\n"
+	BOLD "flags:\n" RESET
+	"\t-tN: sets thread count to N\n"
+	"\t-v:  shows version\n"
+	"\t-D:  disable all script defined `system()` calls";
+	exit(1);
+}
+
 int main(int argc, char *argv[])
 {
 	std::ios_base::sync_with_stdio(false); // because SPEED
 
 	if (argc < 2)
 	{
-		std::cout << "\x1b[34mCate " CATE_VERSION "\x1b[0m\nusage: cate [FILENAME]\nflags: -tNUMBER: sets thread count to NUMBER`";
-		return 1;
+		help();
 	}
 
 	char*file_name;
@@ -28,13 +38,29 @@ int main(int argc, char *argv[])
 		
 		if (arg[0]== '-')
 		{
-			if (arg[1] == 't')//if starts with
+			switch (arg[1])
 			{
+			case 't':
 				if (arg.length() < 3) //if just "-t"
-					Util::fatal_error(0, "Invalid argument " + arg);
+					Util::command_error("Invalid argument " + arg);
 				else
 					thread_count = std::stoi(arg.substr(2, arg.length()));
+				break;
+
+			case 'v':
+				std::cout << CATE_VERSION "\n";
+				return 0;
+				break;
+			
+			case 'D':
+				system_allowed = false;
+				break;
+			
+			default:
+				Util::command_error("Unknown argument " + arg);
+				break;
 			}
+
 		}
 		else
 		{
@@ -48,8 +74,10 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	if (file_name == NULL)
+		Util::command_error("No input file");
+
 	Parser parser(file_name);
-	parser.parse();
 
 	return 0;
 }
