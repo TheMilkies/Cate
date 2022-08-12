68fb55ece11c6aa540b46114e03a2c626b0af1a3
KrystalKX <themilkiesyes@gmail.com>
2022-08-12 16:19:49 +0300

    fancier command-line output stuff

--- src/Library.cpp
+++ src/Library.cpp
@@ -25,10 +25,16 @@ void Library::build()
 	string command;
 	command.reserve(512);
 	if (is_static)
+	{
 		command = "ar rcs -o " + out_name + " " + all_object_files + " ";
+		build_type = " (static)";
+	}
 	else
+	{
 		command = compiler + " -shared -fpic -o " + out_name + " " + all_object_files + final_flags + " " + flags;
+		build_type = " (dynamic)";
+	}
 	
 	Util::system(command);
-	std::cout << "\x1B[32mDone building \"" << name << "\"\u001b[0m\033[0m\n";
+	std::cout << "\x1B[32mDone building \"" << name + build_type << "\"\u001b[0m\033[0m\n";
 }
