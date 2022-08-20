e918d161550fac3dff32d86d3968233376f47b75
Yogurt <themilkiesyes@gmail.com>
2022-08-20 09:54:21 +0300

    fixed color names, i'm colorblind and messed it up

--- src/Util.hpp
+++ src/Util.hpp
@@ -18,8 +18,9 @@ extern int lexer_line;
 #define RED "\033[31m"
 #define GREEN "\033[32m"
 #define YELLOW "\033[33m"
-#define CYAN "\033[34m"
-#define ORANGE "\033[35m"
+#define BLUE "\033[34m"
+#define PURPLE "\033[35m"
+#define CYAN "\033[36m"
 
 namespace Util
 {
