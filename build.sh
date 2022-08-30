7488f43aa750b9d16c89bbd3db11b4346c0cefad
Yogurt <themilkiesyes@gmail.com>
2022-08-30 12:12:48 +0300

    updated build scripts, clang support yay!

--- build.sh
+++ build.sh
@@ -10,36 +10,26 @@ build_folder="cate/build"; mkdir -p $build_folder
 cflags="-fpermissive -funsafe-math-optimizations -ffast-math -fno-signed-zeros -ffinite-math-only -std=c++17 -lstdc++fs -Wall -O3 -pthread -ffunction-sections -fdata-sections -Wl,--gc-sections -fno-ident -fomit-frame-pointer -fmerge-all-constants -Wl,--build-id=none -static"
 
 build_() {
-    g++ src/$1.cpp $cflags -c -o $build_folder/back_src_$1.o
+    g++ src/$1.cpp $cflags -c -o $build_folder/back_src_$1.o 
 }
 
-if ! build_ main; then
-    exit
-fi
+_build() {
+    if ! build_ $1; then
+        exit
+    fi
+}
 
-if ! build_ Lexer; then
-    exit
-fi
+_build Lexer 
 
-if ! build_ Class; then
-    exit
-fi
+_build Class 
 
-if ! build_ Parser; then
-    exit
-fi
+_build Parser 
 
-if ! build_ Util; then
-    exit
-fi
+_build Util
 
-if ! build_ Library; then
-    exit
-fi
+_build Library
 
-if ! build_ Project; then
-    exit
-fi
+_build Project
 
 g++ $build_folder/*.o externals/linux_libfl.a $cflags -oout/cate
 strip -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id --remove-section=.note.ABI-tag out/cate
