e40ae4f4b3d41dcab2e395f46e852c6547bf8cb6
Yogurt <themilkiesyes@gmail.com>
2022-09-05 01:46:33 +0300

    changed release build script

--- build_release.sh
+++ build_release.sh
@@ -9,11 +9,13 @@ sed -i 's/(Development)/(Release)/g' src/Util.hpp #change Development to Release
 touch src/main.cpp # to make sure it changes
 if cate release_smol; then
 	sed -i 's/(Release)/(Development)/g' src/Util.hpp #change it back
+	cd release
 	echo "cp -f cate /usr/bin/cate" > install.sh #generate the install file
 	chmod +x install.sh #add permissons
-	tar -czvf out/linux_cate_v$1.tar.gz out/cate install.sh #create tar
-	zip -9 out/linux_cate_v$1.zip cate install.sh #create zip
-	rm install.sh cate #clean
+	tar -czvf linux_cate_v$1.tar.gz cate install.sh #create tar
+	zip -9 linux_cate_v$1.zip cate install.sh #create zip
+	rm install.sh #clean
+	cd ..
 else
 	sed -i 's/(Release)/(Development)/g' src/Util.hpp #change it back
 	echo "Build error, bye bye"
