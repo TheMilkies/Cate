a68d1a09166adffed0c53da72e810fb430bb85c0
Yogurt <themilkiesyes@gmail.com>
2022-09-15 11:33:49 +0300

    made build.sh a bit better

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #this script is for when you don't have cate or make installed
 
-CC=g++
+CC=cc
 
 if ! command -v $CC &> /dev/null ; then
     echo "no C++ compiler installed."
@@ -10,7 +10,7 @@ fi
 
 build_folder="cate/build"; mkdir -p $build_folder
 out_exec="out/cate"; mkdir -p out
-cflags="-fpermissive -funsafe-math-optimizations -ffast-math -fno-signed-zeros -ffinite-math-only -std=c++17 -lstdc++fs -Wall -O3 -pthread -ffunction-sections -fdata-sections -Wl,--gc-sections -fno-ident -fomit-frame-pointer -fmerge-all-constants -Wl,--build-id=none"
+cflags="-lstdc++ -march=native -fpermissive -funsafe-math-optimizations -ffast-math -fno-signed-zeros -ffinite-math-only -std=c++17 -lstdc++fs -Wall -O3 -pthread -ffunction-sections -fdata-sections -Wl,--gc-sections -fno-ident -fomit-frame-pointer -fmerge-all-constants -Wl,--build-id=none"
 
 build_() {
     $CC src/$1.cpp $cflags -c -o $build_folder/src_$1.o
@@ -35,6 +35,6 @@ _build Project &
 wait < <(jobs -p)
 
 $CC $build_folder/*.o $cflags -o$out_exec externals/linux_amd64_libfl.a
-if command -v strip &> /dev/null ; then
+if command -v strip &> /dev/null; then
     strip -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id --remove-section=.note.ABI-tag out/cate
 fi
