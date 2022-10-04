6bb238f0f213b282904f22069f500ef49eaa2817
Yogurt <themilkiesyes@gmail.com>
2022-10-04 12:52:50 +0300

    better project command thing

--- src/Project.cpp
+++ src/Project.cpp
@@ -6,8 +6,6 @@ Project::~Project() {}
 
 void Project::build() 
 {
-	//same as library
-
 	if (!already_built)
 	{
 		setup();
@@ -16,14 +14,19 @@ void Project::build()
 
 	if (link)
 	{
-		if (!Util::file_exists(out_name.c_str())) //if file doesn't exist
-		needs_rebuild = true;
+		//if file doesn't exist
+		if (!Util::file_exists(out_name.c_str()))
+			needs_rebuild = true;
 
 		if (!needs_rebuild) return;
-	
-		string command = compiler + " -o " + out_name + " " + all_object_files + flags + " " + all_library_paths + all_libraries + " " + all_include_paths + final_flags;
 
-		Util::system(command);
+		Util::system(
+			//command = $CC -o$OUT_NAME $OBJ $FLAGS $LIB_PATHS $LIBS $INCS $FINAL_FLAGS
+			compiler + " -o" + out_name + " " +
+			all_object_files + flags + " " +
+			all_library_paths + all_libraries + " " +
+			all_include_paths + final_flags
+		);
 	}
 	
 	std::cout << GREEN "Done building \"" << name << "\"" COLOR_RESET "\n";
