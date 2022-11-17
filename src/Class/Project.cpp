fd1a95f49c61cce53125c76a042214dec9c8911f
Yogurt <yogurt@themilkies>
2022-11-17 13:22:58 +0200

    added comment

--- src/Class/Project.cpp
+++ src/Class/Project.cpp
@@ -51,7 +51,7 @@ done:
 
 void Project::single_file_build()
 {
-	if(!already_built) setup();
+	//if(!already_built) setup();
 	string& file = files[0];
 	if(newer_than(out_name, file) || force_rebuild)
 	{
