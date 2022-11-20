3345a339945b9275cd05632b2af8f25a60bff821
Yogurt <yogurt@themilkies>
2022-11-20 17:31:20 +0200

    added modern subcate

--- examples/project_examples/basic_project/src/main.c
+++ examples/project_examples/basic_project/src/main.c
@@ -4,14 +4,11 @@
 int main(int argc, char* argv[])
 {
 	puts("Simple project!\n\nTesting function from mmath.c: ");
-	int result = add(2, 3);
-	if(result == 5)
-	{
+
+	if(add(2, 3) == 5)
 		puts("Passed test!");
-	}
 	else
-	{
 		puts("It failed. How?");
-	}
+	
 	return 0;
 }
