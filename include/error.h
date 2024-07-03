96a1a78689424bf1412dc27938cababa430f59ee
Ayin Sonu <sonu@core>
2024-07-03 19:07:52 +0300

    Add the error system

---
+++ include/error.h
@@ -0,0 +1,33 @@
+#ifndef CATE_ERROR_H
+#define CATE_ERROR_H
+#include <stdlib.h>
+#include <stdarg.h>
+
+void cate_error(const char* fmt, ...);
+void cate_error_va(const char* fmt, va_list l);
+void cate_warn(const char* text);
+void cate_error_line(size_t line, const char* fmt, ...);
+void cate_error_line_va(size_t line, const char* fmt, va_list l);
+void cate_warn_line(size_t line, const char* text);
+
+#define BOLD "\033[1m" 
+#define COLOR_RESET "\033[0m"
+
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define PURPLE "\033[35m"
+#define CYAN "\033[36m"
+#define BOLD_RED "\033[1;31m"
+#define BOLD_GREEN "\033[1;32m"
+
+#define list_color CYAN
+#define hl_func(x) YELLOW x COLOR_RESET "()"
+#define hl_var(x) PURPLE x COLOR_RESET
+#define hl_flag(x) "\033[1;32m\t" x COLOR_RESET
+#define traffic_light(good, sep, bad) GREEN good YELLOW sep RED bad COLOR_RESET
+#define choose_light(good, sep, bad) BLUE good RED sep PURPLE bad COLOR_RESET
+#define SUDO "\033[1;33msudo\033[1;34m "
+
+#endif // CATE_ERROR_H
---
+++ src/error.c
@@ -0,0 +1,39 @@
+#include "error.h"
+#include "target.h"
+#include <stdio.h>
+
+void cate_error(const char* fmt, ...) {
+    fprintf(stderr, BOLD_RED"Error: " COLOR_RESET);
+    va_list arg;
+    va_start(arg, fmt);
+    vfprintf(stderr, fmt, arg);
+    fprintf(stderr, NL);
+}
+
+void cate_error_va(const char* fmt, va_list l) {
+    fprintf(stderr, BOLD_RED"Error: "COLOR_RESET);
+    vfprintf(stderr, fmt, l);
+    fprintf(stderr, NL);
+}
+
+void cate_error_line_va(size_t line, const char* fmt, va_list l) {
+    fprintf(stderr, BOLD_RED"Error in line %zu: "COLOR_RESET, line);
+    vfprintf(stderr, fmt, l);
+    fprintf(stderr, NL);
+}
+
+void cate_error_line(size_t line, const char* fmt, ...) {
+    va_list arg;
+    va_start(arg, fmt);
+    cate_error_line_va(line, fmt, arg);
+}
+
+void cate_warn(const char* text) {
+    fprintf(stderr, BOLD YELLOW "Warning: " COLOR_RESET "%s" NL, text);
+}
+
+void cate_warn_line(size_t line, const char* text) {
+    fprintf(stderr, BOLD YELLOW "Warning in line %zu: "
+        COLOR_RESET "%s" NL,
+        line, text);
+}
