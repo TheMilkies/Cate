6632ee232cb1742a5c946e1f02aa4bc15d789481
Ayin Sonu <osdevidiot@proton.me>
2024-07-08 11:05:37 +0300

    Disable catel for now

--- include/catel.h
+++ include/catel.h
@@ -1,11 +1,13 @@
 #ifndef CATEL_H
 #define CATEL_H
 #include <vendor/string_view.h>
+#include "target.h"
 
 #define CATEL_PATH_MAX 256
 #define CATEL_PATH_SIZE 256+5 //5 for the .cate
 typedef struct {
     char dir[CATEL_PATH_SIZE], file[CATEL_PATH_SIZE];
+    char file_path[PATH_MAX];
 } CatelValues;
 
 void catel_init(CatelValues* c);
--- src/catel.c
+++ src/catel.c
@@ -9,33 +9,54 @@ static void catel_parse(CatelValues* c, string_view* text);
 static void error(const char* fmt, ...);
 void cate_help(int exit_code);
 
+//BROKEN, DO NOT USE
+//TODO: Fix catel
 void catel_init(CatelValues* c) {
+    strncpy(c->file, "build.cate", 11);
+
     if(cate_sys_file_exists("cate")) {
+        if(cate_sys_file_exists("cate/build.cate")) {
+            strncpy(c->file_path, "cate/build.cate", 16);
+        }
         strncpy(c->dir, "cate/", 6);
     } else {
+        if(cate_sys_file_exists("build.cate")) {
+            strncpy(c->file_path, "build.cate", 11);
+        }
         strncpy(c->dir, "./", 3);
     }
 
-    if(!cate_sys_file_exists(".catel")) {
-        if(cate_sys_file_exists("cate/build.cate")) {
-            strncpy(c->dir, "cate/build.cate", 16);
-            return;
-        } else if(cate_sys_file_exists("build.cate")) {
-            strncpy(c->dir, "build.cate", 11);
-            return;
-        }
+    if(!cate_sys_file_exists(".catel") && !c->file_path[0]) {
         cate_error("no catefile provided, have some help");
         cate_help(1);
     }
 
-    string_view file = {0};
-    int err = sv_load_file(&file, ".catel");
-    if(err)
-        error("can't open catel file because: %s", strerror(errno));
+    return;
 
-    catel_parse(c, &file);
+    // string_view file = {0};
+    // int err = sv_load_file(&file, ".catel");
+    // if(err)
+    //     error("can't open catel file because: %s", strerror(errno));
 
-    free(file.text);
+    // catel_parse(c, &file);
+
+    // if(c->file_path[0])
+    //     memset(c->file_path, 0, PATH_MAX);
+    // //build the string, the cate extension is added by the parser
+    // {
+    //     const size_t dir_length = strlen(c->dir);
+    //     const size_t name_length = strlen(c->file);
+
+    //     const size_t length = dir_length + 1 + name_length;
+    //     if(length >= PATH_MAX)
+    //         error("catel-created path is too long");
+
+    //     strncpy(c->file_path, c->dir, dir_length);
+    //     strncpy(c->file_path, "/", 2);
+    //     strncpy(c->file_path, c->file, name_length);
+    // }
+
+    // free(file.text);
 }
 
 static void catel_parse(CatelValues* c, string_view* text) {
@@ -70,8 +91,7 @@ static void catel_parse(CatelValues* c, string_view* text) {
             strncpy(prop, v.text, v.length);
 
             if(prop == c->file &&
-                !sv_ends_with(&(string_view) {.length = v.length, .text = prop},
-                    ".cate", 5)) {
+                !sv_ends_with(&v, ".cate", 5)) {
                 strncat(prop, ".cate", 6);
             }
             prop = 0;
