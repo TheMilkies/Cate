0d2ffc1cc047ecb7a533f185ec193a1b5566fbdd
Ayin Sonu <osdevidiot@proton.me>
2024-12-04 07:39:50 +0200

    Time to make it into a library I can finally do stuff like I want to!

--- include/target.h
+++ include/target.h
@@ -1,30 +1,6 @@
 #ifndef CATE_TARGET_H
 #define CATE_TARGET_H
-#include <vendor/string_view.h>
-#include <stdint.h>
 
-typedef struct CateOSTarget {
-    string_view dynamic_ending;
-    string_view static_ending;
-    string_view executable_ending;
-    string_view object_ending;
-    size_t os_name_count;
-    string_view os_names[];
-} CateOSTarget;
-
-extern CateOSTarget cate_target;
-
-uint8_t cate_platform_check(const string_view* id);
-
-#ifdef _WIN32
-#define path_sep '\\'
-#define path_sep_str "\\"
-#define NL "\r\n"
-#else
-#define path_sep '/'
-#define path_sep_str "/"
 #define NL "\n"
-#endif // OS newline check
-#define path_sep_str_len __PP_STRLEN(path_sep_str)
 
 #endif // CATE_TARGET_H
