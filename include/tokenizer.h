a64acaafe3a5421cf41c8238669d1f62512086d9
Ayin Sonu <osdevidiot@proton.me>
2025-03-10 18:01:59 +0200

    [tokenizer] Fix comment

--- include/tokenizer.h
+++ include/tokenizer.h
@@ -29,7 +29,9 @@ enum {
     
     TOK_COUNT_SIZE,
 };
+
 typedef uint8_t TokenKind;
+typedef uint32_t TokenID;
 
 typedef struct {
     //16 million lines should be more than enough for cate.
@@ -41,15 +43,13 @@ typedef da_type(Token) TokensArray;
 /*
 	Only two kinds (identifiers and strings) have a value, so the rest will
 	have a null field, this is not ideal because we'd be wasting cache lines.
-	We have a separate array of only the values:
-		 0      1
-		.link = true;
+	
+	We have a separate array instead!
 	We need to keep track of the current token and the current value, which is
 	just incrementing two integers (see parser).
 	Getting the value is a bit more annoying but we can fit 16 tokens in a
 	cache line and we don't waste (1.5 * token_count * 16) bytes.
 */
-typedef uint32_t TokenID;
 typedef da_type(string_view) TokenValuesArray;
 
 void cate_tokenize(string_view *line, TokensArray *tokens,
